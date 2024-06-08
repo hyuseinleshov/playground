85c436dcfd249a456f23616e11291d029ed95a09
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-08 15:55:22 +0300

    Refactor to work with double values

--- vcs-test/src/Calculator.java
+++ vcs-test/src/Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
-    public int add(int a, int b) {
+    public double add(double a, double b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public double subtract(double a, double b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public double multiply(double a, double b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
-        return a / b;
+    public String divide(double a, double b) {
+        return String.format("%.2f", a / b);
     }
 }

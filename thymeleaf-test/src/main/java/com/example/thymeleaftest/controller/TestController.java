78ba9e34583ba1f692562f5140a165f23c9cf5ee
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-08 23:16:47 +0300

    Test different expressions

--- thymeleaf-test/src/main/java/com/example/thymeleaftest/controller/TestController.java
+++ thymeleaf-test/src/main/java/com/example/thymeleaftest/controller/TestController.java
@@ -1,16 +1,48 @@
 package com.example.thymeleaftest.controller;
 
+import com.example.thymeleaftest.dto.UserDto;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class TestController {
     @GetMapping("/")
     public String showTestPage(Model model) {
-        String variable = "Just another test :)";
-        model.addAttribute("variable", variable);
+//        String variable = "Just another test :)";
+//        model.addAttribute("variable", variable);
 
-        return "test";
+        List<String> items = Arrays.asList("t1", "t2", "t3");
+        model.addAttribute("items", items);
+
+//        boolean isValid = false;
+//        model.addAttribute("condition", isValid);
+
+//        model.addAttribute("color", "#0000FF");
+
+//        model.addAttribute("userDto", new UserDto());
+
+//        model.addAttribute("date", new Date());
+//        model.addAttribute("number", 30.34235345);
+
+        UserDto user = new UserDto();
+        user.setName("Pesho");
+        model.addAttribute("user", user);
+
+        model.addAttribute("userDto", new UserDto());
+
+        return "example";
+    }
+
+    @PostMapping("/")
+    public String getUserName(UserDto userDto) {
+        return "example";
     }
 }
---
+++ thymeleaf-test/src/main/java/com/example/thymeleaftest/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.example.thymeleaftest.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDto {
+    private String name;
+}

7c214f627d3c9c9162a99ef9ba24b1cb993eae39
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-10 17:04:39 +0300

    Test

--- vcs-test/src/Main.java
+++ vcs-test/src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println(calc.subtract(5, 3));
         System.out.println(calc.multiply(5, 3));
         System.out.println(calc.divide(5, 3));
+
+        System.out.println("test");
     }
 }

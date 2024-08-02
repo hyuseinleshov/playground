64611fcd60c4731fc87f72510abfe2f524483d0f
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-08-02 20:36:46 +0300

    Refactor docker-compose and application .yaml and create new test endpoints to persist data

---
+++ docker/src/main/java/com/example/docker/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.docker.controller;
+
+import com.example.docker.entity.User;
+import com.example.docker.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ docker/src/main/java/com/example/docker/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.docker.repository;
+
+import com.example.docker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
